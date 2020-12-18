b1f46f04202ed15984a3338ac61ec30799afacf5
Jared Seefried <jared.seefried@yahoo.com>
2020-12-18 12:19:02 -0700

    Added React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,26 @@
 import React from "react";
-// import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import Home from '../src/pages/Home'
+import Navigation from '../src/components/Navigation'
+import Footer from './components/Footer'
+import Portfolio from './pages/Portfolio'
+import Contact from './pages/Contact'
 
 
 
 
 function App() {
   return (
-   <>
-   <Home />
-   </>
+    <Router>
+    <div className="App">
+      <Navigation />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/portfolio" component={Portfolio} />
+      <Route exact path="/contact" component={Contact} />
+      <Footer />
+    </div>
+  </Router>
   );
 }
 
