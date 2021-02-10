f58a7f624bb4a3da92dc7415d13aa60b5dae9f9b
Jared Seefried <jared.seefried@yahoo.com>
2021-02-10 11:41:26 -0700

    Fixing folders

--- routes/api/contacts.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const contactsController = require("../../controller/contactController");
-
-router.route("/")
-  .get(contactsController.findAll)
-  .post(contactsController.create)
-
-module.exports = router
--- routes/api/index.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const contactRoutes = require("./contacts");
-const mailRoutes = require('./sendMail')
-
-router.use("/contacts", contactRoutes);
-router.use('/sendMail', mailRoutes)
-
-module.exports = router;
