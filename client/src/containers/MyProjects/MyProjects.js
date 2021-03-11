b14c17239bd71be331a4e3af7e21d3657e816ca0
Jared Seefried <jared.seefried@yahoo.com>
2021-03-11 12:56:31 -0700

    fixed meta

--- client/src/containers/MyProjects/MyProjects.js
+++ client/src/containers/MyProjects/MyProjects.js
@@ -1,23 +1,38 @@
+// Dependencies
 import React from 'react';
-import ProjectCard from '../../components/ProjectCard/ProjectCard';
-import WebAppDev from '../../images/full-stack-developer-main.jpg'
 
+// Components
+import ProjectCard from '../../components/ProjectCard/ProjectCard';
+
+// Images
+import NoteApp from '../../images/note-app.jpg'
+import Covid from '../../images/pandemic-tracker.jpg'
+import ChatApp from '../../images/chat app.jpg'
+import EmployeeDirectory from '../../images/employee-directory.jpg'
+import EatDaBurger from '../../images/eat-da-burger.jpg'
+import Weather from '../../images/weather-dashboard.jpg'
+import ShelfIndulgence from '../../images/shelf-indulgence.jpg'
+import MealMatcher from '../../images/meal-matcher.jpg'
+
+// CSS
 import './MyProjects.css'
 
 function MyProjects() {
 
   const projects = [
     {
-      src: WebAppDev,
-      className: '234',
-      id: '123',
+      src: NoteApp,
+      className: 'note-app',
+      id: 'note-app',
       title: 'Note Application',
       text: 'This Note application is build using React for the front end and uses "Serverless" framework to API to Amazon Web Services. The configuration allows a user to sign up and login and while logged in, users can save notes with a file attachment. AWS Congnito is used for user access management, Lambda functions access the AWS API Gateway to make CRUD, (Create, Read, Update and Delete), actions to collection in DynamoDB and an s3 bucket for the attachment. The users home page will render the notes saved in the database',
-      tech: '',
-      alt: 'alt text'
+      tech: 'Amazon Web Services IAM Cognito S3 DynamoDB Amplify and Netlify | Serverless | React.js | Node.js ',
+      alt: 'Note Application',
+      site: 'https://my-serverless-note-app-to-aws.netlify.app/',
+      github: 'https://github.com/jaredseefried/serverless-note-app',
     },
     {
-      src: WebAppDev,
+      src: Covid,
       className: '234',
       id: '123',
       title: 'Pandemic Tracker',
@@ -26,7 +41,7 @@ function MyProjects() {
       alt: 'alt text'
     },
     {
-      src: WebAppDev,
+      src: ChatApp,
       className: '234',
       id: '123',
       title: 'Chat Application',
@@ -35,7 +50,34 @@ function MyProjects() {
       alt: 'alt text'
     },
     {
-      src: WebAppDev,
+      src: EmployeeDirectory,
+      className: '234',
+      id: '123',
+      title: 'Employee Directory',
+      text: 'This real-time chat app is a Node.js application which uses Express.js and Socket.io. Socket.IO is a library that enables real-time, bidirectional and event-based communication between the browser and the server. The client will try to establish a WebSocket connection if possible, and will fall back on HTTP long polling if not.',
+      tech: '',
+      alt: 'alt text'
+    },
+    {
+      src: EatDaBurger,
+      className: '234',
+      id: '123',
+      title: 'Eat-Da-Burger',
+      text: 'This real-time chat app is a Node.js application which uses Express.js and Socket.io. Socket.IO is a library that enables real-time, bidirectional and event-based communication between the browser and the server. The client will try to establish a WebSocket connection if possible, and will fall back on HTTP long polling if not.',
+      tech: '',
+      alt: 'alt text'
+    },
+    {
+      src: Weather,
+      className: '234',
+      id: '123',
+      title: 'Weather Dashboard',
+      text: 'This real-time chat app is a Node.js application which uses Express.js and Socket.io. Socket.IO is a library that enables real-time, bidirectional and event-based communication between the browser and the server. The client will try to establish a WebSocket connection if possible, and will fall back on HTTP long polling if not.',
+      tech: '',
+      alt: 'alt text'
+    },
+    {
+      src: ShelfIndulgence,
       className: '234',
       id: '123',
       title: 'Chat Application',
@@ -44,34 +86,7 @@ function MyProjects() {
       alt: 'alt text'
     },
     {
-      src: WebAppDev,
-      className: '234',
-      id: '123',
-      title: 'Chat Application',
-      text: 'This real-time chat app is a Node.js application which uses Express.js and Socket.io. Socket.IO is a library that enables real-time, bidirectional and event-based communication between the browser and the server. The client will try to establish a WebSocket connection if possible, and will fall back on HTTP long polling if not.',
-      tech: '',
-      alt: 'alt text'
-    },
-    {
-      src: WebAppDev,
-      className: '234',
-      id: '123',
-      title: 'Chat Application',
-      text: 'This real-time chat app is a Node.js application which uses Express.js and Socket.io. Socket.IO is a library that enables real-time, bidirectional and event-based communication between the browser and the server. The client will try to establish a WebSocket connection if possible, and will fall back on HTTP long polling if not.',
-      tech: '',
-      alt: 'alt text'
-    },
-    {
-      src: WebAppDev,
-      className: '234',
-      id: '123',
-      title: 'Chat Application',
-      text: 'This real-time chat app is a Node.js application which uses Express.js and Socket.io. Socket.IO is a library that enables real-time, bidirectional and event-based communication between the browser and the server. The client will try to establish a WebSocket connection if possible, and will fall back on HTTP long polling if not.',
-      tech: '',
-      alt: 'alt text'
-    },
-    {
-      src: WebAppDev,
+      src: MealMatcher,
       className: '234',
       id: '123',
       title: 'Chat Application',
