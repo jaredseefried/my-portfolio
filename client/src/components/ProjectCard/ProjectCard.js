dae9b2a742f176e16970b9c4b5b722a1214f14a4
Jared Seefried <jared.seefried@yahoo.com>
2021-03-11 12:46:40 -0700

    Updating styles and index.html, fixed project card

--- client/src/components/ProjectCard/ProjectCard.js
+++ client/src/components/ProjectCard/ProjectCard.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from 'react-bootstrap/Card';
 import Tilt from 'react-parallax-tilt';
+import Modal from 'react-bootstrap/Modal'
 
 import './ProjectCard.css'
 
 function ProjectCard(props) {
+  const [modalShow, setModalShow] = useState(false);
+
   return (
     <Tilt className='tilt'>
-      <Card className='project-card'>
+      <Card className='project-card' onClick={() => setModalShow(true)}>
 
         <Card.Img
           variant="top"
@@ -21,6 +24,11 @@ function ProjectCard(props) {
           <Card.Title>{props.title}</Card.Title>
         </Card.Body>
       </Card>
+      <Modal
+        description={props.description}
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
     </Tilt>
   );
 }
