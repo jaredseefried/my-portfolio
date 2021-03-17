fdce71a0992bea83cb23e81e191f15715be2e455
Jared Seefried <jared.seefried@yahoo.com>
2021-03-17 10:56:11 -0600

    Fixed icons and removed Parallax

--- client/src/components/ProjectCard/ProjectCard.js
+++ client/src/components/ProjectCard/ProjectCard.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
-import Tilt from 'react-parallax-tilt';
 import MyModal from 'react-bootstrap/Modal'
 
 import './ProjectCard.css'
@@ -9,20 +8,18 @@ function ProjectCard(props) {
 
   return (
     <>
-      <Tilt className='tilt'>
-        <Card className='project-card' onClick={props.onClick}>
-          <Card.Img
-            variant="top"
-            src={props.src}
-            alt={props.alt}
-            className={props.className}
-            id={props.className + '-' + props.id} />
-          <div className="overlay"></div>
-          <Card.Body>
-            <Card.Title>{props.title}</Card.Title>
-          </Card.Body>
-        </Card>
-      </Tilt>
+      <Card className='project-card' onClick={props.onClick}>
+        <Card.Img
+          variant="top"
+          src={props.src}
+          alt={props.alt}
+          className={props.className}
+          id={props.className + '-' + props.id} />
+        <div className="overlay"></div>
+        <Card.Body>
+          <Card.Title>{props.title}</Card.Title>
+        </Card.Body>
+      </Card>
       <MyModal
         {...props}
       />
